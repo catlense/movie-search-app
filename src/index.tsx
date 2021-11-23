54187e53641a1fd72eefe6f9386a2b69e108feb1
lxqtpr <forfollower13@gmail.com>
2021-11-23 16:47:40 +0400

    init commit]

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux";
-import {setupStore} from "./store/store";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { setupStore } from "./store/store";
 
-const store = setupStore ()
+const store = setupStore();
 
-ReactDOM.render (
-    <Provider store={store}>
-        <App/>
-    </Provider>,
-    document.getElementById ( 'root' )
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
-reportWebVitals ();
+reportWebVitals();
