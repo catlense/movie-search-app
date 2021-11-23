cd3e7513b65208df7a0c7ad6862710db532eff26
lxqtpr <forfollower13@gmail.com>
2021-11-23 23:53:17 +0400

    rtk query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React  from "react";
+import {filmAPI} from "./services/FilmService";
 
 const App = () => {
-  return <div></div>;
+    // const dispatch = useAppDispatch ()
+    // const {films} = useAppSelector ( state => state.filmReducer )
+    // useEffect ( () => {
+    //     dispatch ( fetchPopularFilms() )
+    // }, [] )
+    const {data: PopularFilms , error, isLoading } = filmAPI.useFetchPopularFilmsQuery('a6ef9edc1b078d1c39013abfbd9d8509')
+    console.log (PopularFilms && PopularFilms.results, error, isLoading)
+    return <div>
+
+    </div>;
 };
 
 export default App;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,15 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import {filmAPI} from "../services/FilmService";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers ( {
+    [filmAPI.reducerPath]: filmAPI.reducer
+} );
 
 export const setupStore = () => {
-  return configureStore({
-    reducer: rootReducer,
-  });
+    return configureStore ( {
+        reducer: rootReducer,
+        middleware: ( getDefaultMiddleware) => getDefaultMiddleware ().concat ( filmAPI.middleware )
+    } );
 };
 
 export type RootState = ReturnType<typeof rootReducer>;
