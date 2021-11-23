f2685a499d266b30ac06b12e74d34028feeb9c65
Maxim Voronin <catlense@voronin.xyz>
2021-11-24 01:24:18 +0700

    Register functional created

---
+++ server/src/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserDto } from './dto/user-register.dto';
+
+@Controller('auth')
+export class AuthController {
+
+  constructor(private authService: AuthService) {}
+
+  @Post('/register')
+  @HttpCode(HttpStatus.CREATED)
+  register(@Body() userData: UserDto) {
+    return this.authService.registerUser(userData)
+  }
+
+}
